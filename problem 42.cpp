c98a33f42778d41e4978da997a63b57ec6b7dab8
salah-sorour-dev <soroursalaheddine@gmail.com>
2024-11-10 11:57:51 +0100

    Count Odd Numbers in Array

---
+++ problem 42.cpp
@@ -0,0 +1,71 @@
+// problem 42.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int RandomNumber(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return RandNum;
+}
+
+void ReadArray(int arr[100], int& arrLength)
+{
+	cout << "\nEnter Number of elements ?\n";
+	cin >> arrLength;
+	for (int i = 0; i < arrLength; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+	for (int i = 0; i < arrLength; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int CountOddsNumberInArray(int arr[100], int arrLength)
+{
+	int Counter = 0;
+	for (int i = 0; i < arrLength; i++)
+	{
+		if (arr[i] % 2 != 0)
+		{
+			Counter++;
+		}
+		return Counter;
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100], arrLength = 0;
+	ReadArray(arr, arrLength);
+
+	cout << "\nArray Elements: ";
+	PrintArray(arr, arrLength);
+
+	cout << "\nOdd Numbers Count is: ";
+	cout << CountOddsNumberInArray(arr, arrLength);
+	cout << endl;
+
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
